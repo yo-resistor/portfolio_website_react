3c1c2ea2ec7d6dafbae7d1982eac23656f30890e
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-17 14:51:32 -0800

    updated hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,54 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/yun-img.png";
+import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import CV from "../../assets/yunsikohm_resume.pdf";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile picture of Yunsik Ohm"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Dark mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Yunsik
+          <br />
+          Ohm
+        </h1>
+        <h2>Software Engineer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="GitHub icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="LinkedIn icon" />
+          </a>
+        </span>
+        <p>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
