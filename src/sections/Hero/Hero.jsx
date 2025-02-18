861cef3fce90d3b862318d0d541bd2029cc26a92
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-17 16:44:12 -0800

    done with styling. don't like the layout that much. will fork brittany's v4 website using vite.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,24 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/yun-img.png";
-import themeIcon from "../../assets/sun.svg";
-import twitterIcon from "../../assets/twitter-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
+import heroImg from "../../assets/yun-img-big.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/yunsikohm_resume.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
@@ -19,6 +31,7 @@ function Hero() {
           className={styles.colorMode}
           src={themeIcon}
           alt="Dark mode icon"
+          onClick={toggleTheme}
         />
       </div>
       <div className={styles.info}>
@@ -32,14 +45,14 @@ function Hero() {
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/yo-resistor" target="_blank">
             <img src={githubIcon} alt="GitHub icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://linkedin.com/in/yohm/" target="_blank">
             <img src={linkedinIcon} alt="LinkedIn icon" />
           </a>
         </span>
-        <p>
+        <p className={styles.description}>
           With a passion for developing modern React web apps for commercial
           businesses.
         </p>
